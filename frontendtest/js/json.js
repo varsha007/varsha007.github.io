f67f9b5488e7ceb0b7538e627cc5f994d07442fa
varsha <varsha.bvu@gmail.com>
2014-03-13 11:27:45 -0400

    FrontEnd test is done

---
+++ frontendtest/js/json.js
@@ -0,0 +1,90 @@
+
+
+$(document).ready(function() {
+
+  function driver_table(data) {
+  alert('inside table');
+  $('table tbody').append(
+    '<tr>' + '<td class="td_box">' +
+      "<h1>"+data['position']+'&#176;'+"</h1>" +
+      '<h5>Puntos</h5>' +
+      '<h2>'+data['points']+'</h2>' + '</td>' +
+    '<td class="flag">'+'<img src='+data['driver']+'></td>' +
+    '<td class="flag">'+'<img src='+data['flag']+'></td>' +
+    '<td class="name">' +
+      '<h5>'+data['country']+'</h5>'+
+      '<h1>'+data['name']+ '</h1>'+
+      '<h2>'+data['team']+'</h2>'+
+    '</td>'+
+    '<td class="td_color">' +
+      '<h5>ULTIMO GP</h5>'+
+      '<h1>'+data['last_year']+'&#176;'+'</h1>'+
+    '</td>'+
+    '<td class="td_color">' +
+      '<h5>VICTORIAS</h5>'+
+      '<h1>'+data['victories']+'</h1>'+
+    '</td>'+
+    '<td class="td_color">' +
+      '<h5>POLES</h5>'+
+      '<h1>'+data['poles']+'</h1>'+
+    '</td>'+
+    '<td class="td_color">' +
+      '<h5>MEJOR PUESTO</h5>'+
+      '<h1>'+data['bpos']+'&#176;'+data['bposmult']+'</h1>'+
+    '</td>'+
+    +'</tr>'
+  )
+}
+
+ $.getJSON('js/ranking.json', function(data) {
+    $.each(data, function(){
+      driver_table(this);
+    })
+  });
+
+  $('#navigation ul a').click(function(){
+    $('#navigation ul a').removeClass('selected');
+    $(this).addClass('selected');
+  });
+
+  $('#navigation li a#tab2').click(function(){
+    console.log('test');
+    $("#content_changer").hide();
+  });
+
+  $('#navigation li a#tab1').click(function(){
+    console.log('test');
+    $("#content_changer").show();
+  });
+
+/************Server Provides Sorted Json Data****************/
+
+  $('#content li .clasification').click(function() {
+    $('table tbody').empty();
+    $('table tbody').replaceWith('<tbody>');
+   $.getJSON('js/ranking.json', function(data) {
+      $.each(data, function(){
+        driver_table(this);
+      })
+    });
+  });
+
+ $('#content li .nombre').click(function() {
+  $('table tbody').empty();
+    $.getJSON('js/names.json', function(data) {
+      $.each(data, function(){
+        driver_table(this);
+      })
+    });
+  });
+
+  $('#content li .nationality').click(function() {
+  $('table tbody').empty();
+    $.getJSON('js/nationality.json', function(data) {
+      $.each(data, function(){
+        driver_table(this);
+      })
+    });
+  });
+
+});
